fac8bfd1a15782978537a3c77d58360b0fb42493
bd82 <shahars@protonmail.com>
2022-11-03 01:49:37 +0200

    chore: reproduce bug

---
+++ api.d.ts
@@ -0,0 +1,12 @@
+export as namespace chevrotain
+
+export declare const VERSION: string
+
+declare class Foo {
+
+  public publicMethod():void
+  protected protectedMethod(): void
+  private privateMethod(): void
+
+  constructor()
+}
